f6910272f93b85a438e27986d5381c9de60f6397
Keylee <lineberry.kelly@gmail.com>
2020-03-01 17:23:31 -0700

    added connection link for JAWSDB in heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 //Installs
 const mysql = require('mysql');
+let connection;
 
 //Create Connection
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Iveg0tal0velybunch0fc0c0nuts',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Iveg0tal0velybunch0fc0c0nuts',
+        database: 'burgers_db'
+    });
+};
 
 //Makes the Connection
 connection.connect(function(error) {
